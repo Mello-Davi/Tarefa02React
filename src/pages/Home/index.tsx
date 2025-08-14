49ce62e1ce210b5651dbd9a023532db31b29f776
Davi Rodrigues de Mello <davirmelo16@gmail.com>
2025-08-13 23:43:08 -0300

    feat: isercao do banner da pagina home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,14 @@
+import banner from "../../assets/banner.png"
+import styles from "./styles.module.css"
 
 export default function Home(){
     return(
-        <></>
+        <div className={styles.mainHome}>
+            <img src={banner} alt="banner de desconto" id={styles.banner}/>
+
+            <div className={styles.livrosPcategoria}>
+
+            </div>
+        </div>
     )
 }
